3aa74a74050cf8f82133bf0ca6bf1165c3c88e6e
Pete Gordon <pete@petegordon.com>
2020-09-13 14:11:24 -0400

    add lighting and change geometry and color--learning materials

--- src/TableObject.js
+++ src/TableObject.js
@@ -1,7 +1,7 @@
-class TableObject {
+export default class TableObject {
   constructor(THREE, scene, camera) {
-    this.geometry = new THREE.BoxGeometry();
-    this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    this.geometry = new THREE.TorusGeometry(1, 0.4, 16, 50);
+    this.material = new THREE.MeshLambertMaterial({ color: 0xff00ff });
     this.cube = new THREE.Mesh(this.geometry, this.material);
     scene.add(this.cube);
 
--- src/TableScene.js
+++ src/TableScene.js
@@ -2,6 +2,7 @@ import { OrbitControls } from "./OrbitControls.js";
 
 export default class TableScene {
   constructor(THREE, window, document) {
+    this.lights = [];
     this.window = window;
     this.THREE = THREE;
     this.scene = new THREE.Scene();
@@ -16,6 +17,20 @@ export default class TableScene {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
 
+    var light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(1, 1, 1);
+    this.lights.push(light);
+    this.scene.add(light);
+
+    var light = new THREE.DirectionalLight(0x002288);
+    light.position.set(-1, -1, -1);
+    this.lights.push(light);
+    this.scene.add(light);
+
+    var light = new THREE.AmbientLight(0x222222);
+    this.lights.push(light);
+    this.scene.add(light);
+
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
   }
   getTHREE() {
@@ -29,6 +44,9 @@ export default class TableScene {
   }
   getControls() {
     return this.controls;
+  }
+  getLights() {
+    return this.lights;
   }
   initAnimate(obj) {
     this.window.requestAnimationFrame((t) => this.animate(obj));
